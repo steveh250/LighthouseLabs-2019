2820e6baa91b806f6dbac9d724a61e4f2d4f47f8
steveh250 <steve.harris@shaw.ca>
2019-11-02 17:13:48 -0700

    Day 1 for 2020

--- Day 1/Day1.js
+++ Day 1/Day1.js
@@ -1,4 +1,28 @@
-// Function to power on ship
-function powerOn() {
-  ship.powerOn = true;
-}
+// Source data for testing
+
+    const volunteers = [
+      'Sally',
+      'Jake',
+      'Brian',
+      'Hamid'
+    ];
+
+
+    const neighbourhoods = [
+      'Central Valley',
+      'Big Mountain',
+      'Little Bridge',
+      'Bricktown',
+      'Brownsville',
+      "Paul's Boutique",
+      'Clay Park',
+      'Fox Nest'
+    ];
+
+// Day 1
+
+ const doorToDoor = (volunteers, neighbourhoods) => {
+
+ 	// Return the number of neighbourhoods each volunteer needs to visit
+	return (neighbourhoods.length / volunteers.length)
+  }
